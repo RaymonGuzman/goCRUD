f194718bbb19dbf78304529a64fc6158f5908d2a
Raymon Fco. Guzmán Domínguez <frankguzman23@gmail.com>
2021-05-11 02:53:07 -0400

    Installing gorilla mux and getting all the person storaged in the database

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module example.com/mymodule
 go 1.16
 
 require (
+	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/joho/godotenv v1.3.0
 	gorm.io/driver/postgres v1.0.8 // indirect
 	gorm.io/gorm v1.21.8
--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"gorm.io/gorm"
 )
 
@@ -13,33 +15,68 @@ type Person struct {
 
 	Name  string
 	Age   int
-	Email string `gorm:"type:varchar(100);unique_index"`
+	Email string `gorm:"type:varchar(100);uniqueIndex"`
 }
 
-var person = &Person{Name: "Raymitos", Age: 26, Email: "raymon@correo.com"}
+// var person = &Person{Name: "Raymitos", Age: 26, Email: "raymon@correo.com"}
 
 func main() {
+
+	// http.HandleFunc()
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", getPeople)
+	router.HandleFunc("/create", createUser).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8081", router))
+
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "We are on HomePage")
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
 	var db *gorm.DB
 	db = connect()
-	db.Migrator().DropTable(&Person{})
+	// db.Migrator().DropTable(&Person{})
 	db.AutoMigrate(&Person{})
-	db.Create(person)
-	fmt.Println("Funcionooo")
 
 	sqlDB, err := db.DB()
 
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	defer sqlDB.Close()
 
-	// http.HandleFunc()
-	http.HandleFunc("/", homePage)
-	log.Fatal(http.ListenAndServe(":8081", nil))
-
+	var person Person
+	json.NewDecoder(r.Body).Decode(&person)
+	createdPerson := db.Create(&person)
+	// db.Create(&person)
+	err = createdPerson.Error
+	if err != nil {
+		json.NewEncoder(w).Encode(err)
+	} else {
+		json.NewEncoder(w).Encode(&person)
+	}
+	// fmt.Print(createdPerson)
+	// json.NewEncoder(w).Encode(&person)
+	// fmt.Fprint(w, "Creating user")
+	// fmt.Print("Creating user")
 }
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "We are on HomePage")
+func getPeople(w http.ResponseWriter, r *http.Request) {
+	var db *gorm.DB
+	db = connect()
+	// db.Migrator().DropTable(&Person{})
+	db.AutoMigrate(&Person{})
+
+	sqlDB, err := db.DB()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer sqlDB.Close()
+	var people []Person
+	db.Find(&people)
+	json.NewEncoder(w).Encode(&people)
+
 }
